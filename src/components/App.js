507951a27d1f63584b2a2addf815c89771b33615
Asheer <asheer1511@gmail.com>
2023-11-18 01:26:05 +0200

    initial logic and docker compose yml

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PhoneNumberGenerator from './PhoneNumberGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Render the PhoneNumberGenerator component */}
+      <PhoneNumberGenerator />
+    </div>
+  );
+}
+
+export default App;
