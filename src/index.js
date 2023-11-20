72c59ecd1fc41414c39b020d16b8efb3bb3a0130
Asheer <asheer1511@gmail.com>
2023-11-21 00:55:41 +0200

    added remaining logic and test files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App'; // Import App.js from the components folder
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
